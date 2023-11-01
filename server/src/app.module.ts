7103f8b3736763e2c523de55972b8e38eef789d6
sariys33 <sariys33@gmail.com>
2023-11-01 18:25:38 +0900

    static serve

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,17 @@
-import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuestModule } from './quest/quest.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { UtilsModule } from './utils/utils.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-    imports: [QuestModule, UserModule, AuthModule, UtilsModule],
+    imports: [QuestModule, UserModule, AuthModule, UtilsModule, ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'client', 'build'),
+    }),],
     controllers: [AppController],
     providers: [AppService],
 })
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { cookieParser } from "cookie-parser"
-const cookieParser = require('cookie-parser');
+import * as cookieParser from "cookie-parser";
+
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.use(cookieParser());
     app.enableCors({
-        origin: 'http://localhost:3000', // 클라이언트 애플리케이션의 출처
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
-        credentials: true, // 인증 정보 전송 허용 (쿠키 사용 시 필요)
+        origin: 'http://localhost:3000',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,
     });
 
     await app.listen(3001);

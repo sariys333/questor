9676989dad1e21a27dfb806eca572f8dc3b890fb
sariys333 <sariys33@naver.com>
2023-11-01 21:22:57 +0900

    serve static

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,19 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { QuestModule } from './quest/quest.module';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { UtilsModule } from './utils/utils.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { QuestModule } from "./quest/quest.module";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
+import { UtilsModule } from "./utils/utils.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
-    imports: [QuestModule, UserModule, AuthModule, UtilsModule, ServeStaticModule.forRoot({
-        rootPath: join(__dirname, '..', 'client', 'build'),
-    }),],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    QuestModule,
+    UserModule,
+    AuthModule,
+    UtilsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "../../", "client", "build"),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-
-export class AppModule { }
+export class AppModule {}
